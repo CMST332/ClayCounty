b50b5b448823c3b433b5f2be3d7a01e5d395d774
Daisukeniisaka <100369897+Daisukeniisaka@users.noreply.github.com>
2023-04-30 00:55:46 -0500

    Add a back button

--- JS/script.js
+++ JS/script.js
@@ -3,158 +3,179 @@
     function map1(){
         const topo = document.getElementById("topo");
         const sec1 =document.getElementById("s1");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec1.style.display = "block"}
+            topo.style.display ="none",sec1.style.display = "block",mBack.style.display = "block"}
     }
 
     function map2(){
         const topo = document.getElementById("topo");
         const sec2 =document.getElementById("s2");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec2.style.display = "block"}
+            topo.style.display ="none",sec2.style.display = "block",mBack.style.display = "block"}
     }
 
     function map3(){
         const topo = document.getElementById("topo");
         const sec3 =document.getElementById("s3");
+        const mBack = document.getElementById("mapBack");
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec3.style.display = "block"}
+            topo.style.display ="none",sec3.style.display = "block",mBack.style.display = "block"}
     }
 
     function map4(){
         const topo = document.getElementById("topo");
         const sec4 =document.getElementById("s4");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec4.style.display = "block"}
+            topo.style.display ="none",sec4.style.display = "block",mBack.style.display = "block"}
     }
 
     function map5(){
         const topo = document.getElementById("topo");
         const sec5 =document.getElementById("s5");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec5.style.display = "block"}
+            topo.style.display ="none",sec5.style.display = "block",mBack.style.display = "block"}
     }
 
     function map6(){
         const topo = document.getElementById("topo");
         const sec6 =document.getElementById("s6");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec6.style.display = "block"}
+            topo.style.display ="none",sec6.style.display = "block",mBack.style.display = "block"}
     }
 
     function map7(){
         const topo = document.getElementById("topo");
         const sec7 =document.getElementById("s7");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec7.style.display = "block"}
+            topo.style.display ="none",sec7.style.display = "block",mBack.style.display = "block"}
     }
 
     function map8(){
         const topo = document.getElementById("topo");
         const sec8 =document.getElementById("s8");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec8.style.display = "block"}
+            topo.style.display ="none",sec8.style.display = "block",mBack.style.display = "block"}
     }
 
     function map9(){
         const topo = document.getElementById("topo");
         const sec9 =document.getElementById("s9");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec9.style.display = "block"}
+            topo.style.display ="none",sec9.style.display = "block",mBack.style.display = "block"}
     }
 
     function map10(){
         const topo = document.getElementById("topo");
         const sec10 =document.getElementById("s10");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec10.style.display = "block"}
+            topo.style.display ="none",sec10.style.display = "block",mBack.style.display = "block"}
     }
 
     function map11(){
         const topo = document.getElementById("topo");
         const sec11 =document.getElementById("s11");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec11.style.display = "block"}
+            topo.style.display ="none",sec11.style.display = "block",mBack.style.display = "block"}
     }
 
     function map12(){
         const topo = document.getElementById("topo");
         const sec12 =document.getElementById("s12");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec12.style.display = "block"}
+            topo.style.display ="none",sec12.style.display = "block",mBack.style.display = "block"}
     }
 
     function map13(){
         const topo = document.getElementById("topo");
         const sec13 =document.getElementById("s13");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec13.style.display = "block"}
+            topo.style.display ="none",sec13.style.display = "block",mBack.style.display = "block"}
     }
 
     function map14(){
         const topo = document.getElementById("topo");
         const sec14 =document.getElementById("s14");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec14.style.display = "block"}
+            topo.style.display ="none",sec14.style.display = "block",mBack.style.display = "block"}
     }
 
     function map15(){
         const topo = document.getElementById("topo");
         const sec15 =document.getElementById("s15");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec15.style.display = "block"}
+            topo.style.display ="none",sec15.style.display = "block",mBack.style.display = "block"}
     }
 
     function map16(){
         const topo = document.getElementById("topo");
         const sec16 =document.getElementById("s16");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec16.style.display = "block"}
+            topo.style.display ="none",sec16.style.display = "block",mBack.style.display = "block"}
     }
 
     function map17(){
         const topo = document.getElementById("topo");
         const sec17 =document.getElementById("s17");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec17.style.display = "block"}
+            topo.style.display ="none",sec17.style.display = "block",mBack.style.display = "block"}
     }
 
     function map18(){
         const topo = document.getElementById("topo");
         const sec18 =document.getElementById("s18");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec18.style.display = "block"}
+            topo.style.display ="none",sec18.style.display = "block",mBack.style.display = "block"}
     }
 
     function map19(){
         const topo = document.getElementById("topo");
         const sec19 =document.getElementById("s19");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec19.style.display = "block"}
+            topo.style.display ="none",sec19.style.display = "block",mBack.style.display = "block"}
     }
 
     function map20(){
         const topo = document.getElementById("topo");
         const sec20 =document.getElementById("s20");
+        const mBack = document.getElementById("mapBack");
     
         if(topo.style.display=="block"){
-            topo.style.display ="none",sec20.style.display = "block"}
+            topo.style.display ="none",sec20.style.display = "block",mBack.style.display = "block"}
     }
+
